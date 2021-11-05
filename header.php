df4af2747d0a9267d135f41ee8c783fc7af64d2b
ERROR-jpg <jayminsrajgor@gmail.com>
2021-11-05 17:14:51 +0530

    trytry

---
+++ header.php
@@ -0,0 +1,15 @@
+<header class="text-gray-600 body-font">
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <a href = "index.php" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
+        <image href="mdn_logo_only_color.png"\></image>
+        </svg>
+        <span class="ml-3 text-xl">TSF BANKING</span>
+      </a>
+      <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+        <a class="mr-5 hover:text-gray-900" class="nav-link" href="index.php">HOME</a>
+        <a class="mr-5 hover:text-gray-900" class="nav_link" href="customer_table.php">VIEW CUSTOMERS</a>
+        <a class="mr-5 hover:text-gray-900" class="nav_link" href="transactions.php">TRANSACTION HISTORY</a>
+      </nav>
+    </div>
+  </header>
